b454a806ae8bc5127accffc67e500557fa253521
Manju929-arch <manjunathmanjuu155@gmail.com>
2026-09-13 19:44:41 +0530

    Dockerize food delivery app

--- fooddelivery/settings.py
+++ fooddelivery/settings.py
@@ -84,18 +84,27 @@ WSGI_APPLICATION = 'fooddelivery.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'food_delivery',
+#         'USER': 'postgres',
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'food_delivery',
-        'USER': 'postgres',
+        'NAME': os.getenv('DB_NAME', 'food_delivery'),
+        'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
